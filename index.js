1604d9e5f8d2f3d6af83dbe41a9aef30ffda066b
Bideo Wego <bideowego@gmail.com>
2017-01-30 17:17:42 -0500

    Add modules

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+var _ = require('lodash');
+var salutations = require('./lib/salutations');
+var logger = require('./lib/logger');
 
+console.log(salutations);
+console.log();
+
+_.each(salutations, function(salutation) {
+  logger.log(salutation);
+});
 
 
 
