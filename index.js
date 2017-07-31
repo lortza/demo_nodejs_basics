119af267b6057e03c4b26f5d42a0b0303353d48a
anne <richardson.ae@gmail.com>
2017-07-31 17:23:17 -0500

    Install and use lodash package

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ var _ = require('lodash');
 var salutations = require('./lib/salutations');
 var logger = require('./lib/logger');
 
-console.log(salutations);
-console.log();
+// console.log(salutations);
+// console.log();
 
 _.each(salutations, function(salutation) {
   logger.log(salutation);
 });
-
-
-
-
-
 
 
 
